a6df265e501558962d9bc4f30e3e5a37095c5fdd
Africa Light Site <m1nsour.gueye77@gmail.com>
2026-07-31 11:56:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -78,3 +78,64 @@ async function genererFacture() {
 
     doc.save("Facture_" + client + ".pdf");
 }
+
+
+
+
+
+
+
+
+
+
+
+const ADMIN_USER = "admin";
+const ADMIN_PASS = "12345";
+
+// Connexion
+function login() {
+    const user = document.getElementById("username").value.trim();
+    const pass = document.getElementById("password").value.trim();
+
+    if (user === ADMIN_USER && pass === ADMIN_PASS) {
+        localStorage.setItem("admin", "true");
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("error").textContent =
+            "Nom d'utilisateur ou mot de passe incorrect.";
+    }
+}
+
+// Protection des pages
+(function () {
+    const page = location.pathname.split("/").pop();
+
+    if (
+        page !== "index.html" &&
+        page !== "" &&
+        localStorage.getItem("admin") !== "true"
+    ) {
+        location.href = "index.html";
+    }
+})();
+
+// Déconnexion
+function logout() {
+    localStorage.removeItem("admin");
+    location.href = "index.html";
+}
+
+
+
+
+function updateDateTime() {
+    const now = new Date();
+
+    document.getElementById("datetime").innerHTML =
+        now.toLocaleDateString("fr-FR") +
+        " " +
+        now.toLocaleTimeString("fr-FR");
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
